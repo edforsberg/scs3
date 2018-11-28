b93a0d6e5de18626cc0de48737b35545181ba069
Erik Forsberg <erikforsberg95@hotmail.com>
2018-11-28 10:51:10 +0100

    ew

--- LivingCrystals2.m
+++ LivingCrystals2.m
@@ -1,16 +1,19 @@
 clc 
 clf
 
-T0 = 4;
-rc = 10;
+T0 = -2;
+rc = 8;
 Dr = 0;
 Dt = 0;
-dt = 0.5; 
-v = 0.2; 
+dt = 1; 
+v = 0.5; 
+rPush = 1.5;
+rCol = 5; 
 xyRange = 100;
 nrSwimmers = 100;
 timeSteps = 1000;
 showPlot = true;
+colormap cool 
 
 xData = [];
 yData = [];
@@ -38,17 +41,34 @@ for i = 1:timeSteps
     for j = 1:nrSwimmers
         Tn = CalculateTorque(swimmers(j), swimmers, rc, T0);
         tVec = [tVec; Tn];        
-    end    
-    
-    for j = 1:nrSwimmers
-        newPos = interact(swimmers(j), tVec(j), dt, xyRange);
-        xData = [xData;newPos(1)];
-        yData = [yData;newPos(2)];
     end
     
+    oldPos = zeros(nrSwimmers,2); 
+    for j = 1:nrSwimmers   
+        oldPos(j,:) = [swimmers(j).xPos swimmers(j).yPos];
+        newPos = interact(swimmers(j), tVec(j), dt, xyRange);        
+    end
+    
+    colVec = []; 
+    distMat = GetDistMat(swimmers);
+    for j = 1:nrSwimmers
+        swimClose = sum(distMat(:,j) < rPush)-1;
+        if(swimClose > 0)
+            if rand < 0.5
+                swimmers(j).xPos = oldPos(j,1);
+            else
+                swimmers(j).yPos = oldPos(j,2);
+            end
+        end        
+        xData = [xData;swimmers(j).xPos];
+        yData = [yData;swimmers(j).yPos];
+        colVec = [sum(distMat(:,j) < rCol); col]; 
+    end
+        
     if showPlot
         clf
-        scatter(xData, yData, 30, 'filled');
+        col
+        scatter(xData, yData, 30, colVec);
         axis([0 xyRange 0 xyRange])
         drawnow
     end
