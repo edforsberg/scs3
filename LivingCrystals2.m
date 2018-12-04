4d08160c5526879523a9c0c03b2ecc6302979202
Erik Forsberg <erikforsberg95@hotmail.com>
2018-12-04 10:17:39 +0100

    new version of push

--- LivingCrystals2.m
+++ LivingCrystals2.m
@@ -7,30 +7,45 @@ Dr = 0;
 Dt  = 0; 
 xyRange = 100; 
 color = 1; 
-v = 1; 
+v = 0.1; 
 dt = 1; 
 rc = 10; 
-T0 = 1; 
+T0 = -1; 
+timeSteps = 10000; 
+rPush = 1; 
+
+
 
 swimmers = [];
 for i = 1:nrSwimmers
     swimmers = [swimmers; Swimmer(Dr,Dt,v, xyRange, color, true)];
 end
-       
+   
+for i = 1:timeSteps
+
 for j=1:nrSwimmers 
     CalculateTorque(swimmers(j), swimmers([1:j-1, j+1:end]), rc, T0);    
 end 
 
 for j = 1:nrSwimmers 
-    interact(swimmers(j), dt, xyRange)
+    interact(swimmers(j), dt, xyRange);
 end
 
-for j = 1:nrSwimmers 
-    
-    push(swimmers, rc);
-    
+for j = 1:nrSwimmers     
+    push(swimmers, rPush);    
 end
 
-xPos = swimmers.xPos; 
-yPos = swimmers.yPos; 
+for j = 1:nrSwimmers    
+    xPos(j) = swimmers(j).xPos;
+    yPos(j) = swimmers(j).yPos;    
+end
+
+clf 
+scatter(xPos, yPos, 30); 
+axis equal 
+xlim([0 xyRange]) 
+ylim([0 xyRange])
+drawnow
+
+end
         
