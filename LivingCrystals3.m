6fbeed5549a88e52b336960bf30ca85e0a8e7109
Erik Forsberg <erikforsberg95@hotmail.com>
2018-12-04 15:59:05 +0100

    first version on 3

---
+++ LivingCrystals3.m
@@ -0,0 +1,80 @@
+clear all
+
+
+
+nrSwimmers = 200;
+nrActive = 30; 
+Dr = 0;
+Dt  = 0;
+xyRange = 100;
+color = 1;
+v = 0.3;
+dt = 1;
+rc = 8;
+T0 = 0.8;
+timeSteps = 1000;
+rPush = 3;
+record = false;
+
+
+
+swimmers = [];
+for i = 1:nrSwimmers
+    swimmers = [swimmers; Swimmer(Dr,Dt,(i < nrActive+1)*v, xyRange, color, true)];    
+end
+
+for t = 1:timeSteps
+    
+    for j=1:nrActive
+        CalculateTorque(swimmers(j), swimmers([1:j-1, j+1:end]), rc, T0);
+    end
+    
+    for j = 1:nrActive
+        interact(swimmers(j), dt, xyRange);
+    end   
+    
+    push(swimmers, rPush);
+    
+    
+    for j = 1:nrActive
+        xPosActive(j) = swimmers(j).xPos;
+        yPosActive(j) = swimmers(j).yPos;
+        xDirActive(j) = xPosActive(j)+cos(swimmers(j).dir);
+        yDirActive(j) = yPosActive(j)+sin(swimmers(j).dir);
+    end
+    
+    for j = nrActive+1:nrSwimmers
+        xPosPassive(j) = swimmers(j).xPos;
+        yPosPassive(j) = swimmers(j).yPos;
+    end
+    
+    clf
+    scatter(xPosActive, yPosActive, 60, 'b');
+    hold on
+    scatter(xPosPassive, yPosPassive, 60, 'r', 'filled');
+    hold on
+    plot([xPosActive;xDirActive], [yPosActive;yDirActive], 'color', 'red');
+    legend({sprintf('Dr = %d, Dt = %d, v = 0.3, T0 = %0f, dt = %d, t = %d',...
+        Dr, Dt, T0, dt, t)}, 'Location','southwest');
+    axis equal
+    xlim([0 xyRange])
+    ylim([0 xyRange])
+    drawnow
+    if record
+        F(t) = getframe;
+    end    
+end
+
+
+
+if record
+    writerObj = VideoWriter('myVideo.avi');
+    writerObj.FrameRate = 10;
+    open(writerObj);
+    for t=1:length(F)
+        frame = F(t) ;
+        writeVideo(writerObj, frame);
+    end
+    close(writerObj);
+end
+
